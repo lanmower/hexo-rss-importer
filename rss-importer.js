7b2591151bf94e7b0d72bc33d118b340afe99096
Gregory Pike <greg@gregpike.ca>
2017-02-27 01:52:35 -0330

    Testing

--- rss-importer.js
+++ rss-importer.js
@@ -1,9 +1,15 @@
+/* jshint node: true */
+'use strict';
+
 // const rssMigrator = require('hexo-migrator-rss');
 
 exports.start = function (hexo) {
 
   hexo.on('ready', function() {
-    console.log('Well look at that.... ');
+    var CronJob = require('cron').CronJob;
+    new CronJob('0 0,15,30,45 * * * *', function() {
+      console.log('You will see this message every 15 minutes - ', moment().format('h:mmA'));
+    });
   });
 
 };
