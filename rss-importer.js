bd969717a82cfa8e054e98c38a245f1e7f5d64c4
Gregory Pike <greg@gregpike.ca>
2017-03-02 00:43:51 -0330

    Use config and add dependencies

--- rss-importer.js
+++ rss-importer.js
@@ -2,15 +2,38 @@
 'use strict';
 
 // const rssMigrator = require('hexo-migrator-rss');
+const moment = require('moment');
+const cmd = require('node-cmd');
 
 exports.start = function (hexo) {
 
   hexo.on('ready', function() {
-    console.log('STARTING');
+
+    if (process.argv.indexOf('generate') >= 0 || process.argv.indexOf('migrate') >= 0) {
+      return;
+    }
+
+    console.log('Starting RSS Importer cronjob');
     var CronJob = require('cron').CronJob;
-    new CronJob('0 0,15,30,45 * * * *', function() {
-      console.log('You will see this message every 15 minutes - ', moment().format('h:mmA'));
-    });
+    // Run every 15 minutes by default
+    var interval = hexo.config.rss_importer.interval || 15;
+
+    new CronJob('0 */' + interval + ' * * * *', function() {
+
+      console.log('You will see this message every ' + interval + ' minutes - ', moment().format('h:mmA'));
+
+      var preventDuplicatesFlag = ' ';
+      if (hexo.config.rss_importer.preventDuplicates) {
+        preventDuplicatesFlag += '--preventDuplicates';
+      }
+
+      for (var i = 0; i < hexo.config.rss_importer.feeds.length; i++) {
+        var feed = hexo.config.rss_importer.feeds[i];
+        cmd.run('hexo migrate rss ' + feed.url + ' --limit ' + feed.limit + preventDuplicatesFlag);
+        cmd.run('hexo generate');
+      }
+
+    }, null, true, 'America/Toronto');
   });
 
 };
