fb0d39efe5d5e9949b2b0bc9f2d8e01aee28c2ef
lanmower <almagestfraternite@gmail.com>
2022-07-22 14:10:04 +0200

    Update rss-importer.js

--- rss-importer.js
+++ rss-importer.js
@@ -2,10 +2,24 @@
 'use strict';
 
 const moment = require('moment');
-
+let exec = require('child_process').exec;
+function os_func() {
+  this.execCommand = function (cmd) {
+      return new Promise((resolve, reject)=> {
+         exec(cmd, (error, stdout, stderr) => {
+           if (error) {
+              reject(error);
+              return;
+          }
+          resolve(stdout)
+         });
+     })
+ }
+}
+var os = new os_func();
 exports.start = function(hexo) {
 
-  hexo.on('ready', function() {
+  hexo.on('ready', async function() {
 
     // Config setup is required
     if (!hexo.config || !hexo.config.rss_importer || !hexo.config.rss_importer.feeds) {
@@ -23,24 +37,35 @@ exports.start = function(hexo) {
     const CronJob = require('cron').CronJob;
     // Run every 15 minutes by default
     let interval = hexo.config.rss_importer.interval || 15;
-    let cron = new CronJob('0 */' + interval + ' * * * *', function() {
+    let job = async function() {
 
       const cmd = require('node-cmd');
       hexo.log.i('You will see this message every ' + interval + ' minutes - ', moment().format('h:mmA'));
 
       let preventDuplicatesFlag = ' ';
       if (hexo.config.rss_importer.preventDuplicates) {
-        preventDuplicatesFlag += '--preventDuplicates';
+        preventDuplicatesFlag += '--skipduplicate';
       }
+      let creatorflag = '';
 
+      let run = async (input)=>{
+        console.log(input);
+        return os.execCommand(input);
+      }
       for (let i = 0; i < hexo.config.rss_importer.feeds.length; i++) {
         let feed = hexo.config.rss_importer.feeds[i];
-        cmd.run('hexo migrate rss ' + feed.url + ' --limit ' + feed.limit + preventDuplicatesFlag);
-        cmd.run('hexo generate');
+        if (feed.creator) {
+          creatorflag += '--creator ' +feed.creator;
+        }
+        await run('hexo migrate rss ' + feed.url + ' --limit ' + feed.limit + preventDuplicatesFlag+ ' ' + creatorflag)
       }
+      await run('hexo generate');
+      await run('cp -r public/* ~/mutagen/client/public/');
+      await run('cd ~/mutagen/client && node uploader.js');
 
-    }, null, true, 'America/Toronto');
-
+    }
+    let cron = new CronJob('0 */' + interval + ' * * * *', job, null, true, 'America/Toronto');
+    job();
     return cron;
   });
 
